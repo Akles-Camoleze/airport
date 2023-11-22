da0dec523d87f4365344da8c6e0d66f291af8f12
Akles Pires Camoleze <aklespires@gmail.com>
2023-11-21 21:25:37 -0300

    Criada função para leitura de aeroportos

--- src/main/java/ifmg/camoleze/application/Main.java
+++ src/main/java/ifmg/camoleze/application/Main.java
@@ -1,7 +1,15 @@
 package ifmg.camoleze.application;
 
+import ifmg.camoleze.entities.Airport;
+import ifmg.camoleze.structs.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filePath = "MalhaAereaUSA.csv";
+        ArrayList<Airport> airports = Airport.readAirportsFromFile(filePath);
+
+        for (Airport airport : airports) {
+            System.out.println(airport);
+        }
     }
 }
--- src/main/java/ifmg/camoleze/entities/Airport.java
+++ src/main/java/ifmg/camoleze/entities/Airport.java
@@ -1,6 +1,10 @@
 package ifmg.camoleze.entities;
 
 import java.util.TimeZone;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import ifmg.camoleze.structs.ArrayList;
 
 public class Airport {
     private String abbreviation;
@@ -8,4 +12,66 @@ public class Airport {
     private TimeZone timeZone;
     private Integer latitude;
     private Integer longitude;
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public void setLatitude(Integer latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Integer longitude) {
+        this.longitude = longitude;
+    }
+
+    public static ArrayList<Airport> readAirportsFromFile(String filePath) {
+        ArrayList<Airport> airports = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.trim().equals("!")) break;
+                if (!line.trim().startsWith("#")) {
+                    String[] values = line.split(";");
+                    Airport airport = createAirportFromValues(values);
+                    airports.add(airport);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(airports.size());
+        }
+
+        return airports;
+    }
+
+    private static Airport createAirportFromValues(String[] values) {
+        Airport airport = new Airport();
+        airport.setAbbreviation(values[0]);
+        airport.setTimeZone(TimeZone.getTimeZone("GMT" + values[1]));
+        airport.setLongitude(Integer.parseInt(values[2]));
+        airport.setLatitude(Integer.parseInt(values[3]));
+        airport.setName(values[4]);
+        return airport;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "abbreviation='" + abbreviation + '\'' +
+                ", name='" + name + '\'' +
+                ", timeZone=" + timeZone +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
 }
