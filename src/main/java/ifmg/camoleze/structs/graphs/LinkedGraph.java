30d01c463965b708e014d23103477d288f1b6395
Akles Pires Camoleze <aklespires@gmail.com>
2023-11-24 22:13:00 -0300

    Trabalhando com vertices no grafo encadeado

--- src/main/java/ifmg/camoleze/structs/graphs/LinkedGraph.java
+++ src/main/java/ifmg/camoleze/structs/graphs/LinkedGraph.java
@@ -4,14 +4,14 @@ import ifmg.camoleze.requirements.Methods;
 import ifmg.camoleze.structs.lists.ArrayList;
 import ifmg.camoleze.structs.map.HashMap;
 
-public class LinkedGraph<K extends Methods, V extends Methods> implements Graph<K, V, HashMap<K, HashMap<K, ArrayList<V>>>> {
+public class LinkedGraph<K extends Methods, V extends Methods> implements Graph<K, V, ArrayList<HashMap<K, ArrayList<V>>>> {
     private final ArrayList<Vertex<K>> vertices;
-    private final HashMap<K, HashMap<K, ArrayList<V>>> edges;
+    private final ArrayList<HashMap<K, ArrayList<V>>> edges;
     private final boolean targeted;
 
     public LinkedGraph(boolean targeted) {
         this.vertices = new ArrayList<>();
-        this.edges = new HashMap<>();
+        this.edges = new ArrayList<>();
         this.targeted = targeted;
     }
 
@@ -20,7 +20,7 @@ public class LinkedGraph<K extends Methods, V extends Methods> implements Graph<
      */
     public LinkedGraph(ArrayList<Vertex<K>> vertices, boolean targeted) {
         this.vertices = vertices;
-        this.edges = new HashMap<>();
+        this.edges = new ArrayList<>();
         this.targeted = targeted;
     }
 
@@ -29,16 +29,18 @@ public class LinkedGraph<K extends Methods, V extends Methods> implements Graph<
         if (!vertices.contains(vertex)) {
             vertices.add(vertex);
         }
-        edges.put(vertex.getData(), new HashMap<>());
+        edges.add(new HashMap<>());
     }
 
     @Override
     public void addEdge(Vertex<K> source, Vertex<K> destination, V value) {
-        addEdgeHelper(source.getData(), destination.getData(), value);
+        addEdgeHelper(source, destination, value);
+
         source.setExitDegree(source.getExitDegree() + 1);
         destination.setEntryDegree(destination.getEntryDegree() + 1);
+
         if (!targeted) {
-            addEdgeHelper(destination.getData(), source.getData(), value);
+            addEdgeHelper(destination, source, value);
             destination.setExitDegree(destination.getExitDegree() + 1);
             source.setEntryDegree(source.getEntryDegree() + 1);
         }
@@ -50,36 +52,41 @@ public class LinkedGraph<K extends Methods, V extends Methods> implements Graph<
     }
 
     @Override
-    public HashMap<K, HashMap<K, ArrayList<V>>> getEdges() {
+    public ArrayList<HashMap<K, ArrayList<V>>> getEdges() {
         return edges;
     }
 
     public void processEdges(EdgeProcessor<K, V> edgeProcessor) {
         String start = "=".repeat(40);
         String end = start.repeat(2) + "=".repeat(14);
-        edges.forEach((source, edges) -> edges.forEach((destin, values) -> {
-            System.out.printf("\n%s[%s para %s]%s\n\n", start, source.showInGraph(), destin.showInGraph(), start);
-            for (V value : values) {
-                edgeProcessor.process(source, value, destin);
-            }
-            System.out.printf("\n%s\n", end);
-        }));
+        for (int i = 0; i < edges.size(); i++) {
+            K source = vertices.get(i).getData();
+            HashMap<K, ArrayList<V>> sourceEdges = edges.get(i);
+
+            sourceEdges.forEach( (destin, values) -> {
+                System.out.printf("\n%s[%s para %s]%s\n\n", start, source.showInGraph(), destin.showInGraph(), start);
+                for (V value : values) {
+                    edgeProcessor.process(source, value, destin);
+                }
+                System.out.printf("\n%s\n", end);
+            });
+        }
     }
 
-    private void addEdgeHelper(K source, K destination, V value) {
-        HashMap<K, ArrayList<V>> sourceEdges = edges.get(source);
+
+    private void addEdgeHelper(Vertex<K> source, Vertex<K> destination, V value) {
+        HashMap<K, ArrayList<V>> sourceEdges = edges.get(vertices.indexOf(source));
         if (sourceEdges == null) {
             sourceEdges = new HashMap<>();
-            edges.put(source, sourceEdges);
+            edges.add(sourceEdges);
         }
 
-        ArrayList<V> values = sourceEdges.get(destination);
+        ArrayList<V> values = sourceEdges.get(destination.getData());
         if (values == null) {
             values = new ArrayList<>();
-            sourceEdges.put(destination, values);
+            sourceEdges.put(destination.getData(), values);
         }
 
         values.add(value);
     }
-
 }
